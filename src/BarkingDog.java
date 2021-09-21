0fdd8d0a2d3de7ec22540f887b30517c48ee8113
eloketobe <sameloke@yahoo.com>
2021-09-21 11:40:23 +0100

    completed 3 tests

---
+++ src/BarkingDog.java
@@ -0,0 +1,7 @@
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (barking && ((hourOfDay >= 0 && hourOfDay < 8) || (hourOfDay > 22 && hourOfDay < 24))) {
+            return true;
+        } else return false;
+    }
+}
