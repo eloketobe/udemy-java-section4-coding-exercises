fc92161f527a64ab1bacd5a8f0948166c1265d17
eloketobe <sameloke@yahoo.com>
2021-10-05 11:39:26 +0100

    completed more codding exercises

---
+++ src/Person.java
@@ -0,0 +1,42 @@
+public class Person {
+    private int age;
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        if (age < 0 || age > 100) this.age = 0;
+        else this.age = age;
+    }
+
+    public boolean isTeen() {
+        if (age > 12 && age < 20) return true;
+        else return false;
+    }
+
+    public String getFullName() {
+        if (firstName.isEmpty() && lastName.isEmpty()) return "";
+        else if (firstName.isEmpty()) return lastName;
+        else if (lastName.isEmpty()) return firstName;
+        else return firstName + " " + lastName;
+    }
+}
